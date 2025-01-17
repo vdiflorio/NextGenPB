53c18bcc993e4a46285f07bd7d9ba1fd502d9a7e
Vincenzo Di Florio <vincenzo.diflorio97@gmail.com>
2025-01-17 16:50:15 +0100

    local_settings ubuntu

---
+++ local_setting/local_settings_ubuntu.mk
@@ -0,0 +1,28 @@
+CPPFLAGS=\
+-I/usr/include/ -I/usr/include/octave-8.4.0/octave/8.4.0/ \
+-I/opt/octave_file_io/include/ \
+-I/opt/nanoshaper/src \
+-I/usr/local/nextgenPB/include -I/usr/local/nextgenPB/addons \
+-I/opt/bimpp/include \
+-I/opt/lis/include \
+-DHAVE_OCTAVE_44 -DOMPI_SKIP_MPICXX -DBIM_TIMING -DUSE_MPI
+
+
+CXXFLAGS= -O2 -mtune=generic
+CXX=mpicxx
+
+LDFLAGS=-L/opt/octave_file_io/lib \
+-L/usr/lib/x86_64-linux-gnu \
+-L/usr/lib/x86_64-linux-gnu/octave/8.4.0/ \
+-L/opt/lis/lib \
+-L/opt/nanoshaper/build_lib \
+-L/opt/bimpp/lib \
+-Wl,-rpath,/opt/nanoshaper/build_lib \
+-Wl,-rpath,/opt/bimpp/lib  \
+-Wl,-rpath,/opt/octave_file_io/lib \
+-Wl,-rpath,/opt/lis/lib
+
+LIBS=-lNanoShaper -lbim -lbimmumps -lbimlis -lbimp4est \
+     -lbimlinalg -llis -ldmumps -lmumps_common \
+     -lscotcherr -lbz2 -lmpi_usempif08 \
+     -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lp4est -lsc
