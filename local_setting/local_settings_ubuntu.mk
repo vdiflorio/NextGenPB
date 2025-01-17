5f4a81b54c58c3d7e8d3a916d9eb5f7e9b1a854c
carlodefalco <carlodefalco@users.noreply.github.com>
2025-01-17 17:01:14 +0100

    Update local_settings_ubuntu.mk
    link liboctave

--- local_setting/local_settings_ubuntu.mk
+++ local_setting/local_settings_ubuntu.mk
@@ -5,6 +5,7 @@ CPPFLAGS=\
 -I/usr/local/nextgenPB/include -I/usr/local/nextgenPB/addons \
 -I/opt/bimpp/include \
 -I/opt/lis/include \
+$(shell mkoctfile --link-stand-alone -p INCFLAGS) \
 -DHAVE_OCTAVE_44 -DOMPI_SKIP_MPICXX -DBIM_TIMING -DUSE_MPI
 
 
@@ -17,12 +18,16 @@ LDFLAGS=-L/opt/octave_file_io/lib \
 -L/opt/lis/lib \
 -L/opt/nanoshaper/build_lib \
 -L/opt/bimpp/lib \
+$(shell mkoctfile --link-stand-alone -p RDYNAMIC_FLAG) \
+$(shell mkoctfile --link-stand-alone -p LDFLAGS) \
 -Wl,-rpath,/opt/nanoshaper/build_lib \
 -Wl,-rpath,/opt/bimpp/lib  \
 -Wl,-rpath,/opt/octave_file_io/lib \
--Wl,-rpath,/opt/lis/lib
+-Wl,-rpath,/opt/lis/lib 
 
 LIBS=-lNanoShaper -lbim -lbimmumps -lbimlis -lbimp4est \
-     -lbimlinalg -llis -ldmumps -lmumps_common \
+     -lbimlinalg \
+     $(shell mkoctfile --link-stand-alone -p OCTAVE_LIBS) \
+     -llis -ldmumps -lmumps_common \
      -lscotcherr -lbz2 -lmpi_usempif08 \
      -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lp4est -lsc
